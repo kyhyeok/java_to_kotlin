2a563efa90895354e5a9d41fae141e6f01b5ac33
김윤혁 <kyhyeok@gmail.com>
2025-08-22 00:49:02 +0900

    변수를 다루는 방법 조금 수정

--- src/variable/JavaMain.java
+++ src/variable/JavaMain.java
@@ -17,8 +17,12 @@ public class JavaMain {
         // 기본형과 참조형의 연산은 기본형으로 변환되어 처리됨, number3는 자동 언박싱됨
 
         final List<Integer> numbers = Arrays.asList(1, 2);
-        numbers.add(3); // 컴파일 에러 발생, final이지만 내부 요소는 변경 가능
+        numbers.add(3); // UnsupportedOperationException 런타임 에러 발생, final이지만 내부 요소는 변경 가능
         // 요점은 final이 객체의 참조를 변경하지 못하게 할 뿐, 객체 내부의 상태는 변경할 수 있다는 것
+
+        /*
+         * 조금 더 풀어서
+         */
 
         Person person = new Person("혁");
         // 객체 인스턴스화를 위해서 new 키워드를 사용
--- src/variable/Main.kt
+++ src/variable/Main.kt
@@ -3,21 +3,21 @@ package variable
 fun main() {
     var number1 = 10L
     //  Java - long number1 = 10L;
-    // 타비을 자동으로 추론해준다.
+    // 타입을 자동으로 추론해준다.
 
     var number2: Long = 10L
     // 타입을 명시적으로 지정할 수 있다.
 
     val number3 = 10L
-    // Java - final long number2 = 10L;
+    // Java - final long number3 = 10L;
 
     val number4: Long = 10L
 
-    // var number5는 컴파일 에러 - 타입을 추론할 수 없을 때는 타입을 명시적으로 지정해야 한다.
-    var number5: Long
-    // println(number5) 초기화 되지 않은 변수는 사용할 수 없다.
-    number5 = 10L
-    println(number5)
+    // var number5 컴파일 에러 - 타입을 추론할 수 없음
+    var number5: Long // 명시적 타입 지정으로 해결
+    // println(number5) 컴파일 에러 - 초기화 전 사용 불가
+    number5 = 10L // 초기화 후
+    println(number5) // 사용 가능
 
     val number6: Long
     number6 = 10L
@@ -25,11 +25,15 @@ fun main() {
     println(number6)
 
     // val 컬렌셕에는 element를 추가할 수 있다
+    // val은 참조를 고정하지만, 객체 내부 상태는 변경 가능
+    val list = mutableListOf(1, 2, 3)
+    list.add(4) // 가능 - 내부 상태 변경
+    // list = mutableListOf(5, 6) 불가능 - 참조 변경
 
     var number7 = 10L
     var number8 = 1_000L
     var number9 = number7 + number8
-    // boxing과 unboxing이 자동으로 이루어진다.
+    // Kotlin에서는 primitive와 wrapper 구분 없이 자동 처리
 
     // var number10 = 1_000L
     // number10 = null
@@ -42,7 +46,8 @@ fun main() {
     var number12: Long? = 1_000L
     number12 = null
     // null 가능 타입은 ?를 붙여서 선언한다.
+    // 사용 시에는 null 체크 필요: number12?.toString()
 
-    var person = Person("혁")
+    val person = Person("혁")
     // 객체 인스턴스화를 위해서 new가 필요하지 않음
 }
